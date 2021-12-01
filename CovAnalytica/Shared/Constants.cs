d982aa1f62adef6eabf4e182480ee95dc022c7c4
Gabriel Freire <gabrielfreiredev@gmail.com>
2021-12-01 21:09:32 +0000

    add csv vaers data url path; decrease file size

--- CovAnalytica/Shared/Constants.cs
+++ CovAnalytica/Shared/Constants.cs
@@ -15,6 +15,8 @@ namespace CovAnalytica.Shared
         public static string VACCINATIONS_BY_AGE_GROUP_URL_PATH = "owid/covid-19-data/master/public/data/vaccinations/vaccinations-by-age-group.csv";
         public static string VACCINATIONS_BY_MANUFACTURER_URL_PATH = "owid/covid-19-data/master/public/data/vaccinations/vaccinations-by-manufacturer.csv";
         public static string COMPLETE_COVID_DATA_URL_PATH = "owid/covid-19-data/master/public/data/owid-covid-data.csv";
+        public static string VAERS_DATA_URL_PATH = "gabrielfreire/CovAnalytica/master/2021VAERSData.csv";
+        public static string VAERS_VAX_URL_PATH = "gabrielfreire/CovAnalytica/master/2021VAERSVAX.csv";
     
         // FILE PATHS
         public static string VACCINATION_LOCATIONS_FILE_PATH = Path.Combine(Directory.GetCurrentDirectory(), "vaccination_locations.csv");
