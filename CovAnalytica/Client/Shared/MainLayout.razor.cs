acc61f97fdd06a9fb9fb5ab2f137564f5f9c5edb
Gabriel Freire <gabrielfreiredev@gmail.com>
2021-12-04 18:24:25 +0000

    start UI dev

---
+++ CovAnalytica/Client/Shared/MainLayout.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CovAnalytica.Client.Shared
+{
+    public partial class MainLayout
+    {
+        private bool _drawerOpen = true;
+
+        private void DrawerToggle()
+        {
+            _drawerOpen = !_drawerOpen;
+        }
+    }
+}
